d7f1759ba9208f92e34bc96fef94eb25f2306a8a
Thomas Frei <mail@thomasfrei.de>
2015-11-04 19:21:14 +0100

    update package.json and add some test

--- test/test-rest.js
+++ test/test-rest.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var assert = require('chai').assert;
 
 describe('REST API', function(){
   it('test connection', function(done){
@@ -9,13 +9,33 @@ describe('REST API', function(){
 
     var options = {
       url:  urljoin(config.rest.host, 'helloWorld'),
-      rejectUnAuthorized: false // TODO certificate needs to be bundled correctly
+      rejectUnauthorized: false, // TODO certificate needs to be bundled correctly
+      auth: config.auth
     };
 
     request.get(options, function(err, res, body){
       assert.equal(err, null);
+      assert.equal(body, 'Hello World!');
+      done();
+    });
+  });
 
-      console.log(body);
+  it('get orders', function(done){
+
+    var urljoin = require('url-join');
+    var request = require('request');
+    var config = require('./../config.js');
+
+    var options = {
+      url:  urljoin(config.rest.host, config.rest.getOrders),
+      rejectUnauthorized: false, // TODO certificate needs to be bundled correctly
+      auth: config.auth
+    };
+
+    request.get(options, function(err, res, body){
+      assert.equal(err, null);
+      body = JSON.parse(body);
+      assert.isArray(body, 'body needs to be an array');
       done();
     });
   });
